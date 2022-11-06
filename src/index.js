e392b21239ed8d852ac7aefb1c190be17fc1f373
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-11-06 16:40:12 +0100

    +

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-// import fetchCountries from "./fetchCountries.js";
+import fetchCountries from "./fetchCountries.js";
 var debounce = require('lodash.debounce');
 import Notiflix from 'notiflix';
 
@@ -9,47 +9,35 @@ const DEBOUNCE_DELAY = 300;
 inputRef.addEventListener('input',debounce(getName, DEBOUNCE_DELAY))
 
 function getName(event) {
-    const country = event.target.value
-    // if (!country) {
-    //     document.querySelector('.country-list').innerHTML = "";
-    //     return
-    // }
-    fetchCountries(`${country}`);
+    const country = event.target.value.trim()
+    if (!country) {
+        document.querySelector('.country-list').innerHTML = "";
+        return
+    }
+    fetchCountries(`${country}`)
+        .then(createdPages)
+        .catch(empty)
 }
 
+function createdPages(data) {
+    let len = data.length;
 
-let len = 0
-let data = {}
-const API = 'https://restcountries.com/v3.1'
-
-function fetchCountries(nameContry) {
-    fetch(`${API}/name/${nameContry}`)
-        .then(response => {
-            if (!response.ok) {
-                Notiflix.Notify.failure('Oops, there is no country with that name');
-                return
-            } else {
-                fetch(`${API}/name/${nameContry}`)
-                .then(response => response.json())
-                .then(data => console.log(data))
-            }
-
-        
-            len = data.length
-            console.log(len);
-            // if (len > 10) {
-            //     Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-            //     return
-            // }
-            // if (len >= 2 && len <= 10) {
-            //     createFromTwoToTenPages(data)
-            //     return
-            //     }
-            // if (len = 1) {
-            //     createOnePage(data)
-            //     return
-            // }
-        })
+    if (len > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        return
+        }
+    if (len >= 2 && len <= 10) {
+        createFromTwoToTenPages(data)
+        return
+        }
+    if (len = 1) {
+        createOnePage(data)
+        return
+    }
+}
+function empty() {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+    document.querySelector('.country-list').innerHTML = "";
 }
 
 function createOnePage(data) {
@@ -57,7 +45,7 @@ function createOnePage(data) {
             <div class="country-info">
                 <span class="js-country_name">
                     <img class="country_img" src="${countries.flags.png}" alt="${countries.name.common}">
-                    ${countries.name.official}
+                    ${countries.name.official }
                 </span>
                 <p class="country_properties"><strong>Capital: </strong>${countries.capital}</p>
                 <p class="country_properties"><strong>Population: </strong>${countries.population}</p>
@@ -72,7 +60,7 @@ function createFromTwoToTenPages(data) {
                 <div class="country-info">
                     <span class="js-country_name">
                         <img class="country_img" src="${countries.flags.png}" alt="${countries.name.common}">
-                        ${countries.name.official}
+                        ${countries.name.official }
                     </span>
                 </div>
                 `).join('')
