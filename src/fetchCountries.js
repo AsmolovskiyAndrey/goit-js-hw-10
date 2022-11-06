d6792a350eca0e3151fb42a6f22729942ba73ecd
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-11-06 17:19:03 +0100

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,11 @@
 export default function fetchCountries(name) {
     const URL = 'https://restcountries.com/v3.1/name/';
-    
+
     return fetch(`${URL}${name}?fields=name,capital,population,flags,languages`)
         .then(responce => {
             if (!responce.ok) {
-                throw Error(res.statusText);
-            }
+                throw new Error(responce.statusText);
+                }
             return responce.json();
             });
 }
